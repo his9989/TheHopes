5aeb752519bf0d60ef7612a1558dd0d76740f181
dlrmswn95 <dlrmswn95@gmail.com>
2019-08-09 02:55:05 +0900

    init

---
+++ PEM/PEM/Models/RawData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PEMServer.Models
+{
+    public class RawData
+    {
+        public DateTime Time { get; set; }
+        public string ObjName  { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        //public double Altitude { get; set; }
+        public double Ele { get; set; }
+        public RawData(DateTime Time, string ObjName, double Latitude, double Longitude, double Ele)
+        {
+            this.Time = Time;
+            this.ObjName = ObjName;
+            this.Latitude = Latitude;
+            this.Longitude = Longitude;
+            this.Ele = Ele;
+        }
+        /*public RawData(DateTime Time, int ObjName, double Latitude, double Longitude, double Altitude, double Ele)
+        {
+            this.Time = Time;
+            this.ObjName = ObjName;
+            this.Latitude = Latitude;
+            this.Longitude = Longitude;
+            this.Altitude = Altitude;
+            this.Ele = Ele;
+        }*/
+    }
+}
---
+++ PEM/PEM/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PEMServer.Models
+{
+    public class User
+    {
+        public string ObjName { get; set; }
+        public string ObjKorName { get; set; }
+        public int Edit { get; set; }
+        public int CluNo { get; set; }
+        public User(string ObjName, string ObjKorName, int Edit, int CluNo)
+        {
+            this.ObjName = ObjName;
+            this.ObjKorName = ObjKorName;
+            this.Edit = Edit;
+            this.CluNo = CluNo;
+        }
+    }
+}
